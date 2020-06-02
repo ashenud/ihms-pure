6e2deb34c6fee70a765f645a6331f211e5da6049
Ashen <udithamal.lk@gmail.com>
2020-06-03 01:29:01 +0530

    doctor included

--- pages/doctor/doc-table.php
+++ pages/doctor/doc-table.php
@@ -1,9 +1,9 @@
-<?php session_start(); ?>
-<?php include('../../php/basic/connection.php'); ?>
-
-<?php if(!isset($_SESSION['doctor_id'])) {	
-	header('location:../../index.php?noPermission=1');
-	}
+<?php 
+session_start();
+include('../../php/basic/connection.php');
+if(!isset($_SESSION['doctor_id'])) {	
+	header('location:/?noPermission=1');
+}
 ?>
 
 
@@ -14,20 +14,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'>
-
-    <!--favicons-->
-    <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../../assets/img/favicon.png">
-
-    <!--fonts and icons-->
-    <link rel="stylesheet" href="../../assets/css/fontawesome/css/all.css">
-    <link rel="stylesheet" href="../../assets/css/unicode-fonts.css">
-    <link rel="stylesheet" href="../../assets/css/material-design-iconic-font.min.css">
-
-    <!--css files-->
-    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../assets/css/animate.css">
-
+    
+    <?php 
+    //favicons
+    include('../../inc/basic/include-dashboard-fav.php');
+    //css
+    include('../../inc/basic/include-dashboard-css.php');
+    ?>
+    
     <link rel="stylesheet" href="../../assets/css/dashboard-style.css">
 
     <title>Infant Health Management System</title>
@@ -46,122 +40,7 @@
         <div class="main-body">
 
             <!-- sidebar menu -->
-            <div class="sidebar-menu">
-                <div class="inner-sidebar-menu">
-
-                    <div class="user-area pb-2 mb-3">
-                        <img src="./img/doctor.png" class="rounded-circle">
-                        <?php
-                            $query00 = "SELECT * FROM doctor WHERE doctor_id='".$_SESSION['doctor_id']."'";
-                            $result00= mysqli_query($conn,$query00);
-                            $row00=mysqli_fetch_assoc($result00);
-                        ?>
-                        <a href="#"> <span><?php echo $row00['doctor_name'];?></span> </a>
-                    </div>
-
-                    <!--sidebar items-->
-                    <ul>
-                        <li>
-                            <a href="doc-dashboard.php" class="text-uppercase">
-                                <span class="icon">
-                                    <i class="fas fa-chart-pie" aria-hidden="true"></i>
-                                </span>
-                                <span class="list">තොරතුරු පුවරුව</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-uppercase" data-toggle="collapse" href="#manage">
-                                <span class="icon">
-                                    <i class="fas fa-users-cog" aria-hidden="true"></i>
-                                </span>
-                                <span class="list">කළමනාකරණය</span>
-                            </a>
-                        </li>
-                        <div class="collapse collapse-manage" id="manage">
-                            <li>
-                                <a href="doc-view-sisters.php" class="text-uppercase drop">
-                                    <span class="icon-active">
-                                        <i class="fas fa-search" aria-hidden="true"></i>
-                                    </span>
-                                    <span class="list">හෙදියන් බලන්න</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="doc-view-babies.php" class="text-uppercase drop">
-                                    <span class="icon">
-                                        <i class="fas fa-search" aria-hidden="true"></i>
-                                    </span>
-                                    <span class="list">ළමුන් බලන්න</span>
-                                </a>
-                            </li>
-                        </div>
-                        <li>
-                            <a href="#" class="text-uppercase active">
-                                <span class="icon">
-                                    <i class="fas fa-table" aria-hidden="true"></i>
-                                </span>
-                                <span class="list">දත්ත වගු</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="doc-inbox.php" class="text-uppercase">
-                                <span class="icon">
-                                    <i class="fas fa-inbox" aria-hidden="true"></i>
-                                    
-                                    <?php 
-                                        $sql001="SELECT COUNT(status) AS unreadSMS FROM doctor_message WHERE status='unread' AND doctor_id='".$_SESSION['doctor_id']."'";
-                                        $run001=mysqli_query($conn,$sql001);
-                                        $row001=mysqli_fetch_assoc($run001);
-                                        $count=$row001['unreadSMS'];
-
-                                        if(0<$count && $count<=9) {
-                                            echo "<span class='badge badge-danger'>";
-                                            echo $count;
-                                            echo "</span>";
-                                        }
-                                        else if($count>9) {
-                                            echo "<span class='badge badge-danger'>";
-                                            echo "9+";
-                                            echo "</span>";
-                                        }
-                                    ?>
-                                    
-                                </span>
-                                <span class="list">එන පණිවිඩ</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="doc-send-messages.php" class="text-uppercase">
-                                <span class="icon">
-                                    <i class="fas fa-envelope" aria-hidden="true"></i>
-                                </span>
-                                <span class="list">පණිවිඩ යවන්න</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <!--end of sidebar items-->
-
-                    <!--normal and mobile hamburgers-->
-                    <div class="hamburger">
-                        <div class="inner-hamburger">
-                            <span class="arrow">
-                                <i class="fas fa-long-arrow-alt-left" aria-hidden="true"></i>
-                                <i class="fas fa-long-arrow-alt-right" aria-hidden="true" style="display: none;"></i>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="mob-hamburger" style="display: none;">
-                        <div class="mob-inner-hamburger">
-                            <span class="mob-arrow">
-                                <i class="fas fa-long-arrow-alt-left" aria-hidden="true" style="display: none;"></i>
-                                <i class="fas fa-long-arrow-alt-right" aria-hidden="true"></i>
-                            </span>
-                        </div>
-                    </div>
-                    <!--end ofnormal and mobile hamburgers-->
-
-                </div>
-            </div>
+            <?php include('inc/sidebar.php'); ?>
             <!-- end of sidebar menu -->
             
             <!-- content -->
@@ -227,33 +106,30 @@
 
 
     <!-- optional JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="../../assets/js/core/jquery.min.js"></script>
-    <script type="text/javascript" src="../../assets/js/core/popper.min.js"></script>
-    <script type="text/javascript" src="../../assets/js/core/bootstrap.min.js"></script>
-
-    <script type="text/javascript" src="../../assets/js/script.js"> </script>
-    <!--end core js files-->
+    <?php
+    //js
+    include('../../inc/basic/include-dashboard-js.php');
+    ?>
 
     <!-- writed scripts -->
+    <script>
+        $(function() {
+            $('.inner-sidebar-menu ul li a.dd-table').addClass('active');
+        }); 
+    </script>
+        
     <script>
         $(document).ready(function() {
             $(".hamburger").click(function() {
                 $(".wrapper").toggleClass("active");
             });
-        });
-    </script>
-
-    <script>
-        $(document).ready(function() {
+            
             $(".mob-hamburger").click(function() {
                 $(".wrapper").toggleClass("mob-active");
-            });
+            });         
         });
     </script>
     <!-- end of writed scripts -->
-
-
 
 </body>
 
---
+++ pages/doctor/inc/sidebar.php
@@ -0,0 +1,116 @@
+<div class="sidebar-menu">
+    <div class="inner-sidebar-menu">
+
+        <div class="user-area pb-2 mb-3">
+            <img src="./img/doctor.png" class="rounded-circle">
+            <?php
+                $query00 = "SELECT * FROM doctor WHERE doctor_id='".$_SESSION['doctor_id']."'";
+                $result00= mysqli_query($conn,$query00);
+                $row00=mysqli_fetch_assoc($result00);
+            ?>
+            <a href="#"> <span><?php echo $row00['doctor_name'];?></span> </a>
+        </div>
+
+        <!--sidebar items-->
+        <ul>
+            <li>
+                <a href="./doc-dashboard.php" class="text-uppercase d-dash">
+                    <span class="icon">
+                        <i class="fas fa-chart-pie" aria-hidden="true"></i>
+                    </span>
+                    <span class="list">තොරතුරු පුවරුව</span>
+                </a>
+            </li>
+            <li>
+                <a class="text-uppercase" data-toggle="collapse" href="#manage" id="manage-users">
+                    <span class="icon">
+                        <i class="fas fa-users-cog" aria-hidden="true"></i>
+                    </span>
+                    <span class="list">කළමනාකරණය</span>
+                </a>
+            </li>
+            <div class="collapse collapse-manage" id="manage">
+                <li>
+                    <a href="doc-view-sisters.php" class="text-uppercase drop d-sis">
+                        <span class="icon-active">
+                            <i class="fas fa-search" aria-hidden="true"></i>
+                        </span>
+                        <span class="list">හෙදියන් බලන්න</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="doc-view-babies.php" class="text-uppercase drop d-baby">
+                        <span class="icon">
+                            <i class="fas fa-search" aria-hidden="true"></i>
+                        </span>
+                        <span class="list">ළමුන් බලන්න</span>
+                    </a>
+                </li>
+            </div>
+            <li>
+                <a href="doc-table.php" class="text-uppercase dd-table">
+                    <span class="icon">
+                        <i class="fas fa-table" aria-hidden="true"></i>
+                    </span>
+                    <span class="list">දත්ත වගු</span>
+                </a>
+            </li>
+            <li>
+                <a href="doc-inbox.php" class="text-uppercase d-inbox">
+                    <span class="icon">
+                        <i class="fas fa-inbox" aria-hidden="true"></i>
+
+                        <?php
+                            $sql001="SELECT COUNT(status) AS unreadSMS FROM doctor_message WHERE status='unread' AND doctor_id='".$_SESSION['doctor_id']."'";
+                            $run001=mysqli_query($conn,$sql001);
+                            $row001=mysqli_fetch_assoc($run001);
+                            $count=$row001['unreadSMS'];
+
+                            if(0<$count && $count<=9) {
+                                echo "<span class='badge badge-danger'>";
+                                echo $count;
+                                echo "</span>";
+                            }
+                            else if($count>9) {
+                                echo "<span class='badge badge-danger'>";
+                                echo "9+";
+                                echo "</span>";
+                            }
+                        ?>
+
+                    </span>
+                    <span class="list">එන පණිවිඩ</span>
+                </a>
+            </li>
+            <li>
+                <a href="doc-send-messages.php" class="text-uppercase d-send">
+                    <span class="icon">
+                        <i class="fas fa-envelope" aria-hidden="true"></i>
+                    </span>
+                    <span class="list">පණිවිඩ යවන්න</span>
+                </a>
+            </li>
+        </ul>
+        <!--end of sidebar items-->
+
+        <!--normal and mobile hamburgers-->
+        <div class="hamburger">
+            <div class="inner-hamburger">
+                <span class="arrow">
+                    <i class="fas fa-long-arrow-alt-left" aria-hidden="true"></i>
+                    <i class="fas fa-long-arrow-alt-right" aria-hidden="true" style="display: none;"></i>
+                </span>
+            </div>
+        </div>
+        <div class="mob-hamburger" style="display: none;">
+            <div class="mob-inner-hamburger">
+                <span class="mob-arrow">
+                    <i class="fas fa-long-arrow-alt-left" aria-hidden="true" style="display: none;"></i>
+                    <i class="fas fa-long-arrow-alt-right" aria-hidden="true"></i>
+                </span>
+            </div>
+        </div>
+        <!--end ofnormal and mobile hamburgers-->
+
+    </div>
+</div>
