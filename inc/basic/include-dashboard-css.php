8c63de95500e522ec382d82bd0d91f851a0a7fba
Ashen Udithamal <udithamal.lk@gmail.com>
2020-06-26 02:10:09 +0530

    basic inc

--- inc/basic/include-dashboard-css.php
+++ inc/basic/include-dashboard-css.php
@@ -1,10 +1,10 @@
 <!--fonts and icons-->
-<link rel="stylesheet" href="../../assets/css/fontawesome/css/all.css">
-<link rel="stylesheet" href="../../assets/css/unicode-fonts.css">
-<link rel="stylesheet" href="../../assets/css/material-design-iconic-font.min.css">
+<link rel="stylesheet" href="/assets/css/fontawesome/css/all.css">
+<link rel="stylesheet" href="/assets/css/unicode-fonts.css">
+<link rel="stylesheet" href="/assets/css/material-design-iconic-font.min.css">
 
 <!--css files-->
-<link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
-<link rel="stylesheet" href="../../assets/css/animate.css">
+<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="/assets/css/animate.css">
 
-<link rel="stylesheet" href="../../assets/css/dashboard-style.css">
+<link rel="stylesheet" href="/assets/css/dashboard-style.css">
